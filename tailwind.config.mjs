b441829586ca76219a926a2133e805e3fcf16c55
Barry Frost <public@barryfrost.com>
2025-07-05 20:46:19 +0100

    Support device dark mode

---
+++ tailwind.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'media', // This enables dark mode based on device preferences
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
